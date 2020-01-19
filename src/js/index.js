ff326f9ec3ffaaa558932566583ae981ea0acb1d
egovelox <egovelox@protonmail.com>
2020-01-19 17:47:54 +0100

    load config

---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+import '../css/styles.css';
+import x from './views/test';
+import y from './models/test';
+alert(`linked views: ${x} and linked models: ${y} to index.js (controller)`);
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const myPostCssPlugins = [require('postcss-simple-vars'),require('postcss-nested'),require('autoprefixer')];
+
+module.exports = {
+    entry: ['babel-polyfill', './src/js/index.js'],
+    output: {
+        filename: 'js/bundled.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {loader: 'babel-loader'}
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader', {loader: 'postcss-loader', options: {plugins: myPostCssPlugins}}]
+            }
+        ]
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({filename: 'index.html', template: './src/index.html'})
+    ]
+}
